c613cea7a7cace5215e5b4796fc7db29a0e7043e
AndersFlaeng <eaa23aaf@students.eaaa.dk>
2024-09-20 12:10:17 +0200

    webp

--- src/components/OmMig.jsx
+++ src/components/OmMig.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../index.css';
 import '../ommig.css';
-import image from '../assets/images/andersflaeng.png';
+import image from '../assets/images/andersflaeng1.webp';
 import image10 from '../assets/images/andersflaeng.webp'
 import image11 from '../assets/images/html.svg';
 import image4 from '../assets/images/css.svg';
